5b1da81cb516056ad2261aebc3d6391fa856813d
John Serra <john@serra.us>
2023-04-13 04:01:53 +0000

    print statement

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ print('Enter your username:')
 username = input()
 print('Enter your passwoord:')
 passwd = input()
-
-print('{username}, your password is {passwd.len()} long.')
+passwd_length = len(passwd)
+print( username + ", your password is " + passwd_length + " long.")
